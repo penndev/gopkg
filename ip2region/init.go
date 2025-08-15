67bd8e6956da37ef421340d47dc13d66cd74a461
penndev <pennilessfor@gmail.com>
2025-08-15 16:08:38 +0800

    fix: 调整region name

---
+++ ip2region/init.go
@@ -0,0 +1,29 @@
+package ip2region
+
+import (
+	_ "embed"
+	"encoding/json"
+	"log"
+
+	"github.com/penndev/gopkg/ip2region/xdb"
+)
+
+var searcher *xdb.Searcher
+
+//go:embed czdb.xdb
+var defaultXDBData []byte
+
+//go:embed region.json
+var defaultRegion []byte
+
+func init() {
+	var err error
+	searcher, err = xdb.NewWithBuffer(defaultXDBData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(defaultRegion, &Region)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
